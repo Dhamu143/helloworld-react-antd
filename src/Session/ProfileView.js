6326b088b79c0dacdd163db1f2ae30b57a5e2646
Nader Toukabri <nader@emiketic.com>
2018-11-09 19:06:55 +0100

    fix ci

--- src/Session/ProfileView.js
+++ src/Session/ProfileView.js
@@ -1,3 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => <div>Profile</div>;
+import * as PropTypes from '../common/proptypes';
+
+// import * as Activity from '../Shared/Activity.state';
+
+// import { $fetchProfile } from '../Auth/state';
+
+const withStore = connect((state) => ({
+  processing: state.Activity.processing,
+  user: state.Auth.user,
+}));
+
+const propTypes = {
+  dispatch: PropTypes.dispatch.isRequired,
+  processing: PropTypes.bool.isRequired,
+  user: PropTypes.object.isRequired,
+};
+
+const Wrapper = (C) => withStore(C);
+
+class ProfileView extends Component {
+  componentDidMount() {
+    // this.props
+    //   .dispatch($fetchProfile())
+    //   .catch((error) => this.props.dispatch(Activity.$toast('failure', error.message)));
+  }
+
+  render() {
+    const { user, processing } = this.props;
+
+    return <div>Profile ... </div>;
+  }
+}
+
+const WrappedProfileView = Wrapper(ProfileView);
+
+WrappedProfileView.propTypes = {
+};
+
+ProfileView.propTypes = {
+  ...WrappedProfileView.propTypes,
+  ...propTypes,
+};
+
+export default WrappedProfileView;
