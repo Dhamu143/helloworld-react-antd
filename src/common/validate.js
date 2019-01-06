fc038e6fdfdd83750b00cdfc49e013c145a06dc1
Nader Toukabri <nader@emiketic.com>
2019-01-06 18:20:38 +0100

    cleanup

--- src/common/validate.js
+++ src/common/validate.js
@@ -9,7 +9,7 @@ export function exec(value, rules) {
     _error = [..._error, rule(value)];
   });
   return _error.length ? _error : null;
-};
+}
 
 export const PASSWORD_LENGTH = 8;
 export const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
