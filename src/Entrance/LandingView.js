24738de1ef26f7556c7378b4a4cf467c03a64be0
Nader Toukabri <nader.toukabri@gmail.com>
2019-04-06 18:44:47 +0100

    update among,  styles, and assets

--- src/Entrance/LandingView.js
+++ src/Entrance/LandingView.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 import { Spin } from 'antd';
 
+import LogoImage from '../assets/logo.png';
+
+import styles from './LandingView.module.css';
+
 function LandingView() {
   return (
-    <div className="-x-fit -x-content-center">
+    <div className={styles.container}>
+      <img src={LogoImage} alt="logo" />
+
       <Spin size="large" />
     </div>
   );
