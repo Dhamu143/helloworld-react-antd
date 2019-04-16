c2b3d864c13fd539bc753a8b3eeb8f78ba6d3ecc
Nader Toukabri <nader@emiketic.com>
2019-04-16 19:41:43 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import bootstrap from './bootstrap';
 import AppContainer from './AppContainer';
 
 // import registerServiceWorker from './registerServiceWorker';
-import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 
 setupStore();
 
@@ -20,6 +19,5 @@ ReactDOM.render(<AppContainer />, document.getElementById('app'));
 bootstrap();
 
 // registerServiceWorker()
-unregisterServiceWorker();
 
 import('pwacompat');
